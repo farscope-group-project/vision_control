7ed9d90b80ff70aef6a1f6086c05dc63845641e9
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2019-04-12 12:46:41 +0100

    modified subscriber node

--- scripts/subscriber.py
+++ scripts/subscriber.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+	#!/usr/bin/python
 import rospy
+import rosservice
 from std_msgs.msg import Bool, String, Int8
 
 # 
@@ -18,29 +19,30 @@ class vision_control_node():
 		# define a publisher
 		self.vision_pub_response = rospy.Publisher('/VisionResponse', String, queue_size =10)
 		#define a subscriber
-		self.vision_control_sub = rospy.Subscriber('/Vision_System_Status', Int8, self.visioncontrolcallback)
+		self.vision_control_sub = rospy.Subscriber('/MainController', ControllerPacket, self.visioncontrolcallback)
 
 	def visioncontrolcallback(self,data):
 		# get the status that the main controller is sending 
-		self.controller_status = data.data
+		self.controller_status = data.McStatus
+		self.item_name = data.ItemName
+		self.shelf_id = data.ShelfId
  		
 		if self.controller_status == 0  :
 			# initialization process
-			self.save_image_stat = exists('/save_images')
-			self.pose_estim_stat = exists('/pose_estimation')
-			if save_image_stat == False or pose_estim_stat == False:
-				print("Services do not exist")
-			else:
+			service_list = rosservice.get_service_list()
+			if (('/save_images') in service_list):
 				print("Initialization completed")
+			else:
+				print("Services do not exist")
 			
-		elif self.controller_status > 0 and self.controller_status <= 14:
+		elif self.controller_status > 0 and self.controller_status <= 15:
 			# take picture
 			print("Taking a picture")
 			
 			rospy.wait_for_service('/save_images', timeout=None) #can specify a timeout in seconds to wait for the server, if timeout is exceeded ROSException is raised
 			save_images = rospy.ServiceProxy('/save_images', srv_save)
 			try:
-				resp1 = save_images([ObjectNames], frameid, shelfid) # arguments to be passed need to be defined (?)
+				resp1 = save_images(self.item_name, self.controller_status, self.shelf_id) # arguments to be passed need to be checked
 				except rospy.ServiceException as exc:
 					print("Service did not process request: " + str(exc)
 	
@@ -48,7 +50,7 @@ class vision_control_node():
 			# calculate item pose
 			print("Calculating Item pose")
 		
-		#update the vision status
+		#update the vision statusexists
 		self.vision_status = self.controller_status
 		self.vision_pub_response.publish(self.vision_status)
 
